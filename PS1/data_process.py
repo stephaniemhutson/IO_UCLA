7e1223329ce95fa0e1091a48714a1db02c5150f2
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2025-01-23 11:25:54 -0800

    Update process data to include brand and brand-store dummies

--- PS1/data_process.py
+++ PS1/data_process.py
@@ -6,30 +6,39 @@ instruments = pd.read_csv('./PS1_Data/OTCDataInstruments.csv', sep='\t')
 dems = pd.read_csv('./PS1_Data/OTCDemographics.csv', sep='\t')
 
 
-# 1. Using OLS with price and promotion as product characteristics.
 data['rev'] = data['sales_'] * data['price_']
 data['total_cost'] = data['cost_'] * data['sales_']
 
 sws = data.groupby(['store', 'week'])['sales_'].sum()
-total_sales = data['sales_'].sum()
-total_sales = data['rev'].sum()
+# total_sales = data['sales_'].sum()
+# total_sales = data['rev'].sum()
 
 data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / sws[(data.loc[:, 'store'][1], data.loc[:, 'week'][1])]
+# data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / data.loc[:, 'count']
 
 # Validate that the data looks similar to the data presented in table i
 # grouped = data.groupby('brand')[['sales_', 'rev', 'total_cost']].aggregate('sum')
 # grouped.loc[:, 'ms'] = grouped.loc[:, 'sales_'] / (total_sales/ 0.62)
-# print(grouped)
+
+
+# add brand dummies
+brands = data['brand'].unique()
+stores = data['store'].unique()
+
+for brand in brands:
+    data[f'brand_{brand}'] = data['brand'] == brand
+    data[f'brand_{brand}'] = data[f'brand_{brand}'].astype(int)
+
+def store_brand(s, store, brand):
+    if (s['brand'] == brand) and (s['store'] == store):
+        return 1
+    else:
+        return 0
+
+
+for brand in brands:
+    for store in stores:
+        data.loc[:, f'brand_{brand}_store_{store}'] = (data.loc[:, 'brand'] == brand) & (data.loc[:, 'store'] == store)
+        data[f'brand_{brand}_store_{store}'] = data[f'brand_{brand}_store_{store}'].astype(int)
 
 data.to_csv('./cleaned_data/data.csv')
-
-
-# 2. Using OLS with price and promotion as product characteristics and
-# brand dummies.
-# 3. Using OLS with price and promotion as product characteristics and
-# store-brand (the interaction of brand and store) dummies.
-# 4. Estimate the models of 1, 2 and 3 using wholesale cost as an instrument.
-# 5. Estimate the models of 1, 2 and 3 using the Hausman instrument (average price in other markets).
-# 6. Using the analytic formula for elasticity of the logit model, compute
-# the mean own-price elasticities for all brand in the market using the
-# estimates in 1, 2 and 3. Do these results make sense? (Discuss)
