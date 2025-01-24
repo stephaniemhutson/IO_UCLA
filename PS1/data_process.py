bc7836c46a4304321505436067a79f0879f31679
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2025-01-23 17:08:30 -0800

    Update data processing to include hausman and dummies

--- PS1/data_process.py
+++ PS1/data_process.py
@@ -13,21 +13,20 @@ sws = data.groupby(['store', 'week'])['sales_'].sum()
 # total_sales = data['sales_'].sum()
 # total_sales = data['rev'].sum()
 
-data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / sws[(data.loc[:, 'store'][1], data.loc[:, 'week'][1])]
-# data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / data.loc[:, 'count']
+# data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / sws[(data.loc[:, 'store'][1], data.loc[:, 'week'][1])]
+data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / data.loc[:, 'count']
 
 # Validate that the data looks similar to the data presented in table i
 # grouped = data.groupby('brand')[['sales_', 'rev', 'total_cost']].aggregate('sum')
 # grouped.loc[:, 'ms'] = grouped.loc[:, 'sales_'] / (total_sales/ 0.62)
 
+# data.set_index(['store', 'week', 'brand'])
 
 # add brand dummies
 brands = data['brand'].unique()
 stores = data['store'].unique()
+weeks = data['week'].unique()
 
-for brand in brands:
-    data[f'brand_{brand}'] = data['brand'] == brand
-    data[f'brand_{brand}'] = data[f'brand_{brand}'].astype(int)
 
 def store_brand(s, store, brand):
     if (s['brand'] == brand) and (s['store'] == store):
@@ -35,10 +34,21 @@ def store_brand(s, store, brand):
     else:
         return 0
 
+data['hausman'] = 0
+
+# data.loc[:, 'hausman'] = data.loc[(data['week'] == data.loc[:, 'week']) & (data['store'] != data.loc[:, 'store']) & (data['brand'] == 1), 'cost_'].mean()
+# print(data)
+
 
 for brand in brands:
+    data[f'brand_{brand}'] = (data['brand'] == brand).astype(int)
     for store in stores:
-        data.loc[:, f'brand_{brand}_store_{store}'] = (data.loc[:, 'brand'] == brand) & (data.loc[:, 'store'] == store)
-        data[f'brand_{brand}_store_{store}'] = data[f'brand_{brand}_store_{store}'].astype(int)
+        col_name = f'brand_{brand}_store_{store}'
+        data[col_name] = 0
+        data.loc[(data['store'] == store) & (data['brand'] == brand), col_name] = 1
 
+        for week in weeks:
+            data.loc[(data['store'] == store) & (data['week'] == week) & (data['brand'] == brand), 'hausman'] = data[(data['store'] != store) & (data['week'] == week) & (data['brand'] == brand)]['price_'].mean()
+
+print(data)
 data.to_csv('./cleaned_data/data.csv')
