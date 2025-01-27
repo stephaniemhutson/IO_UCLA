bd205774514f1f4637e7890c8429b8bc85f09301
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2025-01-26 19:47:35 -0800

    update data process to include the market share of outside option

--- PS1/data_process.py
+++ PS1/data_process.py
@@ -10,35 +10,19 @@ data['rev'] = data['sales_'] * data['price_']
 data['total_cost'] = data['cost_'] * data['sales_']
 
 sws = data.groupby(['store', 'week'])['sales_'].sum()
-# total_sales = data['sales_'].sum()
-# total_sales = data['rev'].sum()
+
 
 # data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / sws[(data.loc[:, 'store'][1], data.loc[:, 'week'][1])]
 data.loc[:, 'ms_by_store_week'] = data.loc[:, 'sales_'] / data.loc[:, 'count']
 
 # Validate that the data looks similar to the data presented in table i
-# grouped = data.groupby('brand')[['sales_', 'rev', 'total_cost']].aggregate('sum')
-# grouped.loc[:, 'ms'] = grouped.loc[:, 'sales_'] / (total_sales/ 0.62)
-
-# data.set_index(['store', 'week', 'brand'])
 
 # add brand dummies
 brands = data['brand'].unique()
 stores = data['store'].unique()
 weeks = data['week'].unique()
 
-
-def store_brand(s, store, brand):
-    if (s['brand'] == brand) and (s['store'] == store):
-        return 1
-    else:
-        return 0
-
-data['hausman'] = 0
-
-# data.loc[:, 'hausman'] = data.loc[(data['week'] == data.loc[:, 'week']) & (data['store'] != data.loc[:, 'store']) & (data['brand'] == 1), 'cost_'].mean()
-# print(data)
-
+ms_naught =  1 - data.groupby(['week','store'])['ms_by_store_week'].aggregate('sum')
 
 for brand in brands:
     data[f'brand_{brand}'] = (data['brand'] == brand).astype(int)
@@ -49,6 +33,8 @@ for brand in brands:
 
         for week in weeks:
             data.loc[(data['store'] == store) & (data['week'] == week) & (data['brand'] == brand), 'hausman'] = data[(data['store'] != store) & (data['week'] == week) & (data['brand'] == brand)]['price_'].mean()
+            data.loc[(data['store'] == store) & (data['week'] == week) & (data['brand'] == brand), 'ms_naught'] = ms_naught[(week, store)]
+
 
 print(data)
 data.to_csv('./cleaned_data/data.csv')
