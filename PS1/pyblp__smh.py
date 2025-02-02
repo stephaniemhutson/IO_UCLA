831b9dcc151494988e7b8c53b27f5f3eb2db50b6
Stephanie Hutson <stephanie.m.hutson+work@gmail.com>
2025-02-01 21:54:54 -0800

    smh pyblp

--- PS1/pyblp__smh.py
+++ PS1/pyblp__smh.py
@@ -4,69 +4,92 @@ import numpy as np
 
 data = pd.read_csv('./cleaned_data/data.csv')
 
-demo = pd.read_csv('./cleaned_data/OTCDemographics.csv')
+# demo = pd.read_csv('./cleaned_data/OTCDemographics.csv')
 
 ivs = pd.read_csv('./cleaned_data/OTCDataInstruments.csv')
 
 
-nodes = {c: 'nodes' + str(int(c[8:]) -1) for c in demo.columns if c[:8] == "hhincome"}
-demo = demo.rename(columns={"store": "market_ids", **nodes})
-demo['weights'] = 0.05
-data = pd.merge(data, ivs, on=['store', 'brand', 'week', 'cost_'])
-data = data.rename(columns={"price_per_50": "prices", "brand": "product_ids", "ms_by_store_week": "shares", "store": "market_ids"})
-# demo['market_ids'] = demo['store'].astype(str) + '_' + demo['week'].astype(str)
-# data['market_ids'] = demo['store'].astype(str) + '_' + demo['week'].astype(str)
-# data.set_index('market_ids')
-# demo.set_index('market_ids')
-
-# demo_vert = pd.DataFrame({'market_ids': [], 'week': [], 'weights': [], 'income': []})
-# for i in range(20):
-#     temp_vert = demo[['market_ids', 'week', f'hhincome{i+1}']].rename(columns={f'hhincome{i+1}': 'income'})
-#     temp_vert['weights'] = 0.05
-#     demo_vert = pd.concat([demo_vert, temp_vert])
-
-# # agent_income_str = " + ".join([c for c in demo.columns if c[:5] == "nodes"])
-# price_store_str = " + ".join([c for c in data.columns if c[:10] == 'pricestore'])
-# initial_sigma = np.eye(11)
+# # nodes = {c: 'nodes' + str(int(c[8:]) -1) for c in demo.columns if c[:8] == "hhincome"}
+# # demo = demo.rename(columns={"store": "market_ids", **nodes})
+# # demo['weights'] = 0.05
+# # data = pd.merge(data, ivs, on=['store', 'brand', 'week', 'cost_'])
+# # data = data.rename(columns={"price_per_50": "prices", "brand": "product_ids", "ms_by_store_week": "shares", "store": "market_ids"})
 
 
-# X1_formulation = pyblp.Formulation('0 + prices', absorb='C(product_ids)')
-# X2_formulation = pyblp.Formulation('1 + prices + prom_')
 
-# product_formulations = (X1_formulation, X2_formulation)
+# # Reshape from wide to long format
+# df_long = demo.melt(id_vars=["store", "week"], var_name="draw", value_name="income")
 
+# # Extract draw number from "hhincome#" column
+# df_long["draw"] = df_long["draw"].str.extract("(\d+)").astype(int)
 
-# mc_integration = pyblp.Integration('monte_carlo', size=50, specification_options={'seed': 0})
-# # pr_integration = pyblp.Integration('product', size=11)
-# mc_problem = pyblp.Problem(
-#     product_formulations=product_formulations,
-#     agent_formulation=agent_formulation,
-#     product_data=data,
-#     agent_data=demo_vert,
-#     integration=mc_integration
-# )
+# # Create a market identifier (store-week)
+# df_long["market_ids"] = df_long["store"].astype(str) + "_" + df_long["week"].astype(str)
 
+# # Rename columns to match pyblp expectations
+# df_long = df_long.rename(columns={"draw": "draw_ids"})
 
-# # pr_problem = pyblp.Problem(product_formulations, data, integration=pr_integration)
-# bfgs = pyblp.Optimization('bfgs', {'gtol': 1e-4})
-# results1 = mc_problem.solve(sigma=np.ones((3, 3)), optimization=bfgs)
-# print(results1)
+# # Select necessary columns
+# df_long = df_long[["market_ids", "draw_ids", "income"]]
 
-# # results2 = pr_problem.solve(sigma=np.ones((3, 3)), optimization=bfgs)
-# # print(results2)
+# # Save for pyblp
+# df_long.to_csv("./cleaned_data/formatted_demographics.csv", index=False)
 
 
-# without Demo data to:
 
-# market_ids, product_ids, firm_ids, shares, prices
-X1 = pyblp.Formulation('1 + C(product_ids) ')
-X2 = pyblp.Formulation('1 + prices')
+
+
+# # Create a unique market-product identifier
+# data["join"] = data["store"].astype(str) + "_" + data["week"].astype(str) + "_" + data["brand"].astype(str)
+# ivs["join"] = ivs["store"].astype(str) + "_" + ivs["week"].astype(str) + "_" + ivs["brand"].astype(str)
+
+# # # Merge on market-product identifier
+# data = data.merge(ivs.drop(columns=["store", "week", "brand", "cost_"]), on="join", how="left")
+# data['market_ids'] = data["store"].astype(str) + "_" + data["week"].astype(str)
+
+# # # Save merged dataset
+# data.to_csv("formatted_data.csv", index=False)
+
+
+
+demographics = pd.read_csv("./cleaned_data/formatted_demographics.csv")
+data = pd.read_csv("./formatted_data.csv")
+instrument_columns = ['cost_', 'avoutprice'] + [f'pricestore{i}' for i in range(1, 31)]
+
+data['demand_instruments33'] = data[instrument_columns].transform(np.size)
+
+data = data.rename(columns={'price_': 'prices', 'ms_by_store_week': 'shares'})
+
+# Linear product characteristics (including price)
+X1_formulation = pyblp.Formulation('1 + prices + ' + ' + '.join([f'brand_{i}' for i in range(2,11)]))  # Includes a constant, price, and brand dummies
+
+# Nonlinear product characteristics (random coefficients)
+X2_formulation = pyblp.Formulation('1 + prices')
+
+# Instrument formulation (exclude price to avoid redundancy)
+Z_formulation = pyblp.Formulation('0 + cost_ + avoutprice + ' + ' + '.join([f'pricestore{i}' for i in range(1, 31)]))
+
+integration = pyblp.Integration('monte_carlo', size=20)
+
+pyblp.build_blp_instruments(Z_formulation, data)
 
 problem = pyblp.Problem(
-    product_formulations=(X1, X2, ),
-    # agent_formulation=agent_formulation,
+    product_formulations=(X1_formulation, X2_formulation),
     product_data=data,
-    agent_data=demo,
-    )
+    agent_data=demographics,
+    integration=integration,
+)
 
-problem.solve(sigma=np.ones([2,2]))
+
+
+instruments = data[instrument_columns].to_numpy()
+W = np.linalg.inv(np.matmul(np.transpose(instruments), instruments))
+print(W.shape)
+
+results = problem.solve(
+    sigma=np.ones([2,2]) + np.eye(2),
+    method='2s',  # Choose an optimization method
+    # W=W
+    # instruments=Z_formulation
+)
+print(results)
